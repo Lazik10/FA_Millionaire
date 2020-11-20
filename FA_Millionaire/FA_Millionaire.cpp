250152276f375fbcaf8c68f1042cf8cd446eaa76
Lázik <LazikL@email.cz>
2020-11-20 13:19:10 +0100

    Add project files.

---
+++ FA_Millionaire/FA_Millionaire.cpp
@@ -0,0 +1,13 @@
+#include "FA_Millionaire.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+void Main(array<String^>^ args) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    //WinformCDemo is your project name
+    FAMillionaire::FA_Millionaire form;
+    Application::Run(% form);
+}
---
+++ FA_Millionaire/FA_Millionaire.h
@@ -0,0 +1,59 @@
+#pragma once
+
+namespace FAMillionaire {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for FA_Millionaire
+	/// </summary>
+	public ref class FA_Millionaire : public System::Windows::Forms::Form
+	{
+	public:
+		FA_Millionaire(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~FA_Millionaire()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = gcnew System::ComponentModel::Container();
+			this->Size = System::Drawing::Size(300,300);
+			this->Text = L"FA_Millionaire";
+			this->Padding = System::Windows::Forms::Padding(0);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+		}
+#pragma endregion
+	};
+}
